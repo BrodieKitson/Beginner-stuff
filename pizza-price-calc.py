70d6f1754ece287f690cf26d40c96a2b9f8a57dc
BrodieKitson <120764936+BrodieKitson@users.noreply.github.com>
2022-12-20 16:44:47 +1030

    Update pizza-price-calc.py

--- pizza-price-calc.py
+++ pizza-price-calc.py
@@ -1,12 +1,9 @@
-# 🚨 Don't change the code below 👇
 print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M, or L ")
 add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
 bill = 0
-# 🚨 Don't change the code above 👆
 
-# Write your code below this line 👇
 if size == "S" and add_pepperoni == "Y" and extra_cheese == "Y":
     bill = 18
     print(f"Your final bill is: ${bill}.")
